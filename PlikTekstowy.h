97fed9c5b924d4dd3cbb2c89d188f0690a38cdad
Mateusz Kadziolka <mateuszkadziolka.programista@gmail.com>
2021-07-26 17:18:02 +0200

    Minor bugs fixes

--- PlikTekstowy.h
+++ PlikTekstowy.h
@@ -14,18 +14,25 @@ class PlikTekstowy
 
 public:
 
-    PlikTekstowy(string nazwaPliku) : NAZWA_PLIKU(nazwaPliku)
-    {
+    PlikTekstowy(string nazwaPliku) : NAZWA_PLIKU(nazwaPliku){
     }
+
 
     bool czyPlikJestPusty()
     {
+        bool pusty = true;
         fstream plikTekstowy;
-        plikTekstowy.seekg(0, ios::end);
-        if (plikTekstowy.tellg() == 0)
-            return true;
-        else
-            return false;
+        plikTekstowy.open(pobierzNazwePliku().c_str(), ios::app);
+
+        if (plikTekstowy.good() == true)
+        {
+            plikTekstowy.seekg(0, ios::end);
+            if (plikTekstowy.tellg() != 0)
+                pusty = false;
+        }
+
+        plikTekstowy.close();
+        return pusty;
     }
 
     string pobierzNazwePliku()
